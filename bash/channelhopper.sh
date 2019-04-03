b648fa313f3b18368cea50a63f73348b3a9e6c05
Marius Zander <zandermarius1@gmail.com>
2019-04-03 16:53:46 +0200

    fixed channelhopper

--- bash/channelhopper.sh
+++ bash/channelhopper.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 DEVICE=$(printenv 'MONITOR_DEVICE') # Get the device from the enviorment.
-chnl=0
-while true; do
+chnl=1
+while :
+do
         sleep $1
         echo "Hopping to channel $chnl"
-        iwconfig $DEVICE channel $(( ($chnl % 14) + 1))
-        ((chnl++))
+        iwconfig $DEVICE channel $chnl
+        chnl=$(( ( ( $chnl ) % 13) + 1 ))
 done
+
