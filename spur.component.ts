f54196974d5afb35504e0bfb9bad4e62a769080f
Sylver0102 <49523299+Sylver0102@users.noreply.github.com>
2019-05-30 17:22:15 +0200

    Create spur.component.ts

---
+++ spur.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-spur',
+  templateUrl: './spur.component.html',
+  styleUrls: ['./spur.component.css']
+})
+export class SpurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
