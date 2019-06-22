844d6cd349e0aa2f1d746c605be7f513d4ef1d27
Marius Zander <zandermarius1@gmail.com>
2019-06-22 16:54:48 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 from math import log10
 
+
+class Circle:
+    def __init__(self, radius, posX,posY):
+        self.radius = radius
+        self.posX = posX
+        self.posY = posY
+    
+
 def dbmToMeter(dBm):
-    frequency = 2417
+    frequency = 2417 #MHz
     fspl = 27.55 # Free-Space Path Loss adapted avarage constant for home WiFI routers and following units
     distance = 10 ** (( fspl - (20 * log10(frequency)) + dBm ) / 20 )
     return round(distance,4)
 
-print(str(dbmToMeter(49)))
+def calculatePosition(circleA, circleB, circleC):
+
+def calculateInterception(circleA,circleB):
+    
